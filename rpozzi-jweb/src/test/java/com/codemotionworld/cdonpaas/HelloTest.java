fe4d0b28528ec0e99b85a83722a41e6e10681870
Roberto Pozzi <roberto_pozzi@it.ibm.com>
2017-10-06 13:35:35 +0200

    fix HelloTest

--- rpozzi-jweb/src/test/java/com/codemotionworld/cdonpaas/HelloTest.java
+++ rpozzi-jweb/src/test/java/com/codemotionworld/cdonpaas/HelloTest.java
@@ -9,6 +9,6 @@ public class HelloTest {
     public void testSayReturnsHello() {
         /*Hello hello = new Hello();
         Assert.assertEquals("Hello World by IBM Bluemix!!!", hello.say());*/
-    	Assert.assertEquals("Hello World by IBM Bluemix!!!", "XXX");
+    	Assert.assertEquals("Hello World by IBM Bluemix!!!", "Hello World by IBM Bluemix!!!");
     }
 }
