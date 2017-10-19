303e527587c087f2af74378140f99b64de52d28b
Mac Oreste <oreste@MacBookOreste.lan>
2017-10-19 11:16:40 +0200

    cancellato directory non usate

--- rpozzi-jweb-db/restaurants/db2/runCompose.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#####################################################
-############## Run Multiple Containers ##############
-#####################################################
-#docker-compose -f docker-compose-all-v1.yml up
-docker-compose -f docker-compose-gateway-v1.yml up
